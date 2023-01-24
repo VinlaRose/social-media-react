afba52a881edcc39d5534ac348077fca6c38cfb0
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-01-24 21:14:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,12 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-import React, {useState} from 'react';
-
-function App(){
-
-  const [count,updateCount] = useState(0);
-  const [theme,updateTheme] = useState("blue")
-
-  function decrement(){
-    console.log("rendering");
-    updateCount(prevcount => prevcount - 1);
-    updateTheme("green");
-    //updateCount(count-1);
-  }
-
-  function increment(){
-    console.log("rendering");
-    updateCount(count+1);
-    updateTheme("red");
-  }
+import Home from "./pages/home/Home"
+function App() {
   return (
-    <>
-    <button onClick={decrement}>-</button>
-    <span>{count}</span>
-    <span>{theme}</span>
-    <button onClick={increment}>+</button>
-    </>
-  )
+    
+        <Home/>
+        
+     
+   
+  );
 }
 
 export default App;
