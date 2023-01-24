6318f72527129ce562e69eac956ccc3a85353c6c
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-01-24 22:41:25 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
+import {Person} from "@mui/icons-material"
 function App() {
   return (
     
-        <Home/>
+        <Person/>
         
      
    
