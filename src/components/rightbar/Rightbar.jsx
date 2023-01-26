bd73f59b0a0cafdf95f398741733341057269704
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-01-26 21:14:02 +0530

    feat:added-online-frnds

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -8,11 +8,24 @@ export default function Rightbar(){
                 <div className="rightBarWrapper">
                     <div className="bdayMsg">
                         <img src="assets/gift.jpg" alt="" className="bdayIcon" />
-                        <span className="bdayText"><b>veronica</b> and <b>3 others </b>have their bday today.</span>
+                        <span className="bdayText"><b>Veronica</b> and <b>3 others </b>have their bday today.</span>
                     </div>
                     <div className="adBox">
                         <img src="assets/add1.jpg" alt="" className="adImg" />
                     </div>
+                    <h4 className='onlineFrndsHeading'>Online Friends</h4>
+                    <ul className="ListonlineFriends">
+                        <li className="onlineFriend">
+                            <div className="profileImageContainer">
+                                <img src="assets/alice.jpg" alt="" className="onlineFriendProfilepic"/>
+                                <span className="onlinebadge"></span>
+
+                            </div>
+                            
+                            <span className="onlineFriendName">Alice Dsouza</span>
+                        </li>
+                    </ul>
+                    
                 </div>
             </div>
         
